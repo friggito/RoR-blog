f5e04933db228a1393f06445fee6f23346fa5a83
friggito <ornella.friggit@gmail.com>
2019-08-07 21:16:15 -0400

    Added articles view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,12 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+    
     def new
     end
     
@@ -8,13 +16,10 @@ class ArticlesController < ApplicationController
         @article.save
         redirect_to @article
     end
-
+\
     private
         def article_params
             params.require(:article).permit(:title, :text)
         end
 
-    # def show
-
-    # end
 end
